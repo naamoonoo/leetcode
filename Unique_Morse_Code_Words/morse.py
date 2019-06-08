ca3ca51420b727dc53c0866bd1d820571886f395
naamoonoo <naamoonoo.y@gmail.com>
2019-06-08 14:07:30 -0700

    morse code translate

---
+++ Unique_Morse_Code_Words/morse.py
@@ -0,0 +1,26 @@
+class Solution:
+	def uniqueMorseRepresentations(self, words):
+		alphas = [".-","-...","-.-.","-..",".","..-.","--.","....","..",
+		".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+		"..-","...-",".--","-..-","-.--","--.."]
+		res = set()
+		for word in words:
+			tmp = ""
+			for c in word:
+				tmp += alphas[ord(c) - 97]
+			res.add(tmp)
+		return len(list(res))
+
+# prettier answer
+# MORSE = [".-","-...","-.-.","-..",".","..-.","--.",
+#                  "....","..",".---","-.-",".-..","--","-.",
+#                  "---",".--.","--.-",".-.","...","-","..-",
+#                  "...-",".--","-..-","-.--","--.."]
+
+#         seen = {"".join(MORSE[ord(c) - ord('a')] for c in word)
+#                 for word in words}
+
+#         return len(seen)
+
+# join with for expression is used well
+print(Solution().uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
